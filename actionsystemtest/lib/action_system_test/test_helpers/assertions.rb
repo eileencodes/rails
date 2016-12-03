e0c7d22b37118fbea292d5252d7cde8a629be27b
eileencodes <eileencodes@gmail.com>
2016-12-03 09:27:44 -0500

    Update Rails provided assertions for selectors
    This didn't quite work right and after debugging a bit I found that
    there was an easier way to express what this was doing. Originally the
    code was extracted from Basecamp 3's system tests.
    
    These assertions assert that all of or none of the selectors on the page
    match the provided items. It iterates through the provided items and
    checks each one rather than requiring the test writer to assert each
    selector manually in their test code.
    
    This is a shortcut. Instead of writing:
    
    ```
    assert_selector :avatar, 'Eileen'
    assert_selector :avatar, 'Jeremy'
    ```
    
    Users can now do
    
    ```
    assert_all_of_selectors :avatar, 'Eileen', 'Jeremy'
    ```

--- actionsystemtest/lib/action_system_test/test_helpers/assertions.rb
+++ actionsystemtest/lib/action_system_test/test_helpers/assertions.rb
@@ -4,37 +4,30 @@ module ActionSystemTest
     # These are assertions that are useful specifically for Rails applications.
     module Assertions
       # Asserts that all of the provided selectors are present on the given page.
+      # Using the selector, +:avatar+ for example, asserts that the selector is
+      # present with the items passed in. If options are provided, the assertion
+      # will check that each item is present with those options as well.
       #
-      #   assert_all_of_selectors('p', 'td')
-      def assert_all_of_selectors(*items)
-        options = items.extract_options!
-        type = type_for_selector(items)
-
+      #   assert_all_of_selectors(:avatar, 'Eileen', 'Jeremy')
+      #   assert_all_of_selectors(:avatar, 'Eileen', 'Jeremy', visible: all)
+      def assert_all_of_selectors(selector, *items, **options)
         items.each do |item|
-          assert_selector type, item, options
+          assert_selector selector, item, options
         end
       end
 
       # Asserts that none of the provided selectors are present on the page.
+      # Using the selector, +:avatar+ for example, asserts that the selector
+      # is not present with the items passed in. If options are provided,
+      # the assertion will check that each item is present with those
+      # options as well.
       #
-      #   assert_none_of_selectors('ul', 'ol')
-      def assert_none_of_selectors(*items)
-        options = items.extract_options!
-        type = type_for_selector(items)
-
+      #   assert_none_of_selectors(:avatar, 'Tom', 'Dan')
+      def assert_none_of_selectors(selector, *items, **options)
         items.each do |item|
-          assert_no_selector type, item, options
+          assert_no_selector selector, item, options
         end
       end
-
-      private
-        def type_for_selector(*items)
-          if items.first.is_a?(Symbol)
-            items.shift
-          else
-            Capybara.default_selector
-          end
-        end
     end
   end
 end
